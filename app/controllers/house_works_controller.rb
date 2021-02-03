d0d5c6892c250429cf1b479b7d113405cf981faf
atsunii <tky3a1938@gmail.com>
2021-02-03 12:30:25 +0900

    画像の追加、css編集

--- app/controllers/house_works_controller.rb
+++ app/controllers/house_works_controller.rb
@@ -2,7 +2,7 @@ class HouseWorksController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @house_works = HouseWork.order("created_at DESC")
+    @house_works = HouseWork.all.order("created_at DESC")
   end
 
   def new
