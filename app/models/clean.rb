c8c8ea6fa0ca59faa806a885c90170a0e3a2e91f
atsunii <tky3a1938@gmail.com>
2021-02-03 12:29:30 +0900

    表示内容の変更

--- app/models/clean.rb
+++ app/models/clean.rb
@@ -1,14 +1,14 @@
 class Clean < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
-    { id: 1, name: 'リビング整頓' },
-    { id: 2, name: 'リビング掃除機がけ'},
-    { id: 3, name: 'キッチンシンク' },
-    { id: 4, name: 'ガスコンロ' },
-    { id: 5, name: 'トイレ' },
-    { id: 6, name: '風呂' },
-    { id: 7, name: '洗面台' },
-    { id: 8, name: '廊下' }
+    { id: 1, name: 'リビング整頓(2点)' },
+    { id: 2, name: 'リビング掃除機がけ(2点)'},
+    { id: 3, name: 'キッチンシンク(2点)' },
+    { id: 4, name: 'ガスコンロ(2点)' },
+    { id: 5, name: 'トイレ(2点)' },
+    { id: 6, name: '風呂(2点)' },
+    { id: 7, name: '洗面台(2点)' },
+    { id: 8, name: '廊下(2点)' }
   ]
   include ActiveHash::Associations
   has_many :house_works
--- app/models/towel.rb
+++ app/models/towel.rb
@@ -1,8 +1,8 @@
 class Towel < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
-    { id: 1, name: 'キッチンタオル' },
-    { id: 2, name: '洗面所タオル' },
+    { id: 1, name: 'キッチンタオル(1点)' },
+    { id: 2, name: '洗面所タオル(1点)' },
   ]
   include ActiveHash::Associations
   has_many :house_works
